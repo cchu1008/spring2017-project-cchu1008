bbb3a65ae5093ac0c841dca9d2699999bd5841b4
Courtney Chu <Courtney Chu@HP>
2017-05-26 15:59:42 -0700

    Added KnightTest

---
+++ test/test/KnightTest.java
@@ -0,0 +1,86 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import main.driver.GameDriver;
+import main.helper.ImageType;
+import main.helper.Position;
+import main.piece.Knight;
+import main.piece.Pawn;
+
+public class KnightTest {
+  Knight knight;
+  ArrayList<Position> moves = new ArrayList<>();
+
+  @Test
+  public void testValidClear() {
+    GameDriver.clearBoard();
+    knight = new Knight(new Position(3, 3), false, ImageType.BLACK_KNIGHT);
+    GameDriver.board[3][3] = knight;
+    
+    moves.add(new Position(2, 5));
+    moves.add(new Position(2, 1));
+    
+    moves.add(new Position(5, 2));
+    moves.add(new Position(1, 2));
+    
+    moves.add(new Position(4, 5));
+    moves.add(new Position(4, 1));
+    
+    moves.add(new Position(5, 4));
+    moves.add(new Position(1, 4));
+    
+    assertEquals(moves, (ArrayList<Position>)(knight.getValid()));
+    
+  }
+  
+  @Test
+  public void testValidBlocked() {
+    GameDriver.clearBoard();
+    Pawn p = new Pawn(new Position(2, 5), false, ImageType.BLACK_PAWN);
+    GameDriver.board[2][5] = p;
+    knight = new Knight(new Position(3, 3), false, ImageType.BLACK_KNIGHT);
+    GameDriver.board[3][3] = knight;
+    
+    moves.add(new Position(2, 1));
+    
+    moves.add(new Position(5, 2));
+    moves.add(new Position(1, 2));
+    
+    moves.add(new Position(4, 5));
+    moves.add(new Position(4, 1));
+    
+    moves.add(new Position(5, 4));
+    moves.add(new Position(1, 4));
+    
+    assertEquals(moves, (ArrayList<Position>)(knight.getValid()));
+  }
+  
+  @Test
+  public void testValidAttack(){
+    GameDriver.clearBoard();
+    Pawn p = new Pawn(new Position(2, 5), true, ImageType.WHITE_PAWN);
+    GameDriver.board[2][5] = p;
+    knight = new Knight(new Position(3, 3), false, ImageType.BLACK_KNIGHT);
+    GameDriver.board[3][3] = knight;
+    
+    moves.add(new Position(2, 5));
+    moves.add(new Position(2, 1));
+    
+    moves.add(new Position(5, 2));
+    moves.add(new Position(1, 2));
+    
+    moves.add(new Position(4, 5));
+    moves.add(new Position(4, 1));
+    
+    moves.add(new Position(5, 4));
+    moves.add(new Position(1, 4));
+    
+    assertEquals(moves, (ArrayList<Position>)(knight.getValid()));
+  }
+
+}
